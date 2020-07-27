d254044d07d45f92c48c44487ef790bd2b019ea7
Zakaria Ben Jaoued <zakaria.benjawed@gmail.com>
2020-07-27 16:20:21 +0100

    Fixing bug

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
-/**import React, { Component } from "react";
-import { Provider } from 'react-redux';
-
-import { HashRouter as Router, Route } from 'react-router-dom';
-import './App.css';
-
-
-import MyNavbar from './containers/myNavbar/MyNavbar'
-
-class App extends Component {
-  render() {
-    return (
-<hi> fzvroguns</hi>
-    );
-  }
-}
-
-export default App;
-
-**/
-
 import React from "react";
 import store from './store/Store';
 import "./App.css";
 import Main from "./containers/Main";
 
 import { Provider } from 'react-redux';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
