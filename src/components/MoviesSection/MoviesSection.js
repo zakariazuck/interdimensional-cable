612ce4ed5005a49db55f2f136afe2fee834ccc03
Zakaria Ben Jaoued <zakaria.benjawed@gmail.com>
2020-07-27 16:14:16 +0100

    movie section

---
+++ src/components/MoviesSection/MoviesSection.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+
+import MovieCard from '../MovieCard/MovieCard';
+
+export class MoviesSection extends Component {
+  render() {
+    const { movies } = this.props;
+    let content = '';
+    content =
+      movies.page >= 1
+        ? movies.results.map((movie, index) => (
+            <MovieCard key={index} movie={movie} />
+          ))
+        : null;
+    return <div className="row">{content}</div>;
+  }
+}
+
+const mapStateToProps = state => ({
+  movies: state.movies.movies
+});
+
+export default connect(mapStateToProps)(MoviesContainer);
