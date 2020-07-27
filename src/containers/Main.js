c9658f4c7e630b57c2c98746d53371c65fbf0601
Zakaria Ben Jaoued <zakaria.benjawed@gmail.com>
2020-07-27 16:08:52 +0100

    Hiding the landing section when search result is loading

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,12 +1,27 @@
-import React, { Component } from "react";
-import MyNavbar from "./myNavbar/MyNavbar";
+import React, { Component } from 'react';
+import MyNavbar from './myNavbar/MyNavbar';
+import SearchResultsSection from './SearchResultsSection/SearchResultsSection';
+import LandingSection from './LandingSection/LandingSection';
 
 export default class Main extends Component {
-    render() {
-      return (
-        <div>
-          <MyNavbar />
-        </div>
-      );
-    }
+  constructor() {
+    super();
+    this.state = {
+      hideLaningSection: false,
+    };
   }
+
+  hideLaningSection() {
+    this.setState({ hideLaningSection: true });
+  }
+
+  render() {
+    return (
+      <div>
+        <MyNavbar data={{ hideLaningSection: this.hideLaningSection.bind(this) }} />
+        { !this.state.hideLaningSection ? <LandingSection /> : null }
+        <SearchResultsSection />
+      </div>
+    );
+  }
+}
